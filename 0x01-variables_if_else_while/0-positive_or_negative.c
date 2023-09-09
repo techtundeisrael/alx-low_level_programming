d8fabb69598ad8fa2e3fb5c8d5e4b028c7647ea1
techtundeisrael <tundefortunate@gmail.com>
2023-09-08 18:35:18 -0700

    Posistive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 /* more headers goes there */
 #include <sdio.h>
 /** betty style doc for function main goes there */
-/** 
+/**
  * main - main function
- * 
+ *
  * Return; always 0
  */
 int main(void)
