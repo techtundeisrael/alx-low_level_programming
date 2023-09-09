2c1580df0c97a6e525852c89bc946ecf22770e54
techtundeisrael <tundefortunate@gmail.com>
2023-09-09 08:28:07 -0700

    Positive or nagative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main: function
+ * main - Entry point
  *
- * return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5", n, m);
 	else if (m == 0)
 		printf("Last digit of %d is %d and is 0", n, m);
-	else if (m > 6 && m !n 0)
+	else if (m > 6 && m) != n 0
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
 	printf("\n");
 
