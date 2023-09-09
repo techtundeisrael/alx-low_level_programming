028ed21bac2f0d0fb93382f63d25c13a0cbf9703
techtundeisrael <tundefortunate@gmail.com>
2023-09-09 05:25:27 -0700

    Positive or nagative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-#include <sdio.h>
+#include <stdio.h>
 /** betty style doc for function main goes there */
 /**
- * main - main function
+ * main - function
  *
  * Return: always 0
  */
@@ -16,11 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		prinf("%d is positive". n);
+		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%d is zero". n);
+		printf("%d is zero", n);
 	else
-		printf("%d is nagative". n);
+		printf("%d is nagative", n);
 	printf("/n");
 	return (0);
 }
