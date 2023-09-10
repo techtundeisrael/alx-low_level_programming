d074390b1b4fd1fc615128414a3b7953f13635d6
techtundeisrael <tundefortunate@gmail.com>
2023-09-10 09:26:18 -0700

    NumberZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+		for (c = '0'; c <= '9'; c++)
+			putchar(c);
+		putchar('\n');
+	return (0);
+}
+
