6d7d1843776941e3a6da280870629136487ed9ae
techtundeisrael <tundefortunate@gmail.com>
2023-09-11 10:35:44 -0700

    Printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return; Always 0 (success)
+ */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",);
-	return (1)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - main function
  *
- * Return; Always 0 (success)
+ * Return: always 0
  */
-
 int main(void)
+
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
