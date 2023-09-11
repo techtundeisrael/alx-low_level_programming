6926015b140091daaa8bb3e639a73cadb237d9a5
techtundeisrael <tundefortunate@gmail.com>
2023-09-11 11:37:27 -0700

    print and return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - main function
  *
- * Return; Always 1 (indicating an error)
+ * Return; Always 1 (success)
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	printf("and that piece of art is useful" "- Dora Korpar, 2015-10-19");
 	return (1);
 }
