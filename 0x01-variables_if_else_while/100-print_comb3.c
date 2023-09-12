122dafbf3c8e79ede1b4e672ff642710c61b3937
techtundeisrael <tundefortunate@gmail.com>
2023-09-11 19:10:21 -0700

    printf different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = '0'; digit1 <= '9'; digit1++)
+	for (digit1 = '0'; digit1 <= '8'; digit1++)
 	{
-		for (digit2 = '1'; digit2 <= '9'; digit2++)
+		for (digit2 = '8' ? '9' : '0'; digit2 <= '9'; digit2++)
 		{
 		putchar(digit1);
 		putchar(digit2);
 
-		for (digit1 != '9' || digit2 != '9')
+		if (digit1 == '8' && digit2 == '9')
 		{
 
 			putchar(',');
