a776bd445c59ca47b83f90c1f96e51102ee924cf
techtundeisrael <tundefortunate@gmail.com>
2023-09-18 16:44:13 -0700

    function that prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,12 +6,13 @@
  * Return: void
  */
 
-void _puts(char *str);
+void _puts(char *str)
+
 {
 
-	for (; *str != '\0; str++)
+	for (; *str != '\0'; str++)
 	{
-	_putchar(*str);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
