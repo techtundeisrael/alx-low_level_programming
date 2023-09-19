a0f6acf8923586574b53bbf213df7880459aea62
techtundeisrael <tundefortunate@gmail.com>
2023-09-19 05:54:45 -0700

    unction that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - Prints a string
@@ -7,12 +8,10 @@
  */
 
 void _puts(char *str)
-
-{
-
+{	
 	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+	putchar(*str);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
